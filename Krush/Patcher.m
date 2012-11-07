cf6561f6ce0e493be2c94721f494e36b1a2a22dd
bsmt <bsmt@krax.in>
2012-11-07 13:29:48 -0700

    Initial wildcard search/replace implemented.

--- Krush/Patcher.m
+++ Krush/Patcher.m
@@ -14,11 +14,26 @@
 {
     Patcher *patch = [[Patcher alloc] init];
     patch.binary = bin;
+    patch.target = bin.path;
     return patch;
 }
 
++(Patcher *)patcherWithFile:(NSURL *)path
+{
+    Patcher *patch = [[Patcher alloc] init];
+    patch.target = path;
+    return patch;
+}
+
+
 -(BOOL)patchSymbol:(NSString *)symbolName withReturnValue:(unsigned int)ret
 {
+    if (![self binary])
+    {
+        // This method requires a MachO binary
+        return FALSE;
+    }
+    
     NSError *error = NULL;
     NSFileHandle *handle = [NSFileHandle fileHandleForWritingToURL:[binary path] error:&error];
 
@@ -45,8 +60,9 @@
 
 -(BOOL)replaceData:(NSData *)search with:(NSData *)replace
 {
-    NSRange searchRange = NSMakeRange(0, [[binary binary] length]);
-    NSRange dataRange = [[binary binary] rangeOfData:search options:0 range:searchRange];
+    NSData *target_contents = [NSData dataWithContentsOfURL:[self target]];
+    NSRange searchRange = NSMakeRange(0, [target_contents length]);
+    NSRange dataRange = [target_contents rangeOfData:search options:0 range:searchRange];
     
     if (dataRange.location == NSNotFound)
     {
@@ -55,7 +71,7 @@
     else
     {
         NSError *error = NULL;
-        NSFileHandle *handle = [NSFileHandle fileHandleForWritingToURL:[binary path] error:&error];
+        NSFileHandle *handle = [NSFileHandle fileHandleForWritingToURL:[self target] error:&error];
         [handle seekToFileOffset:dataRange.location];
         [handle writeData:replace];
         
@@ -64,9 +80,43 @@
     }
 }
 
--(BOOL)replaceData:(NSData *)search with:(NSData *)replace useWildcards:(BOOL)wildcard
+-(BOOL)replaceString:(NSString *)search with:(NSString *)replace
 {
-    return FALSE; // not implemented
+    NSData *search_data = [search dataUsingEncoding:NSASCIIStringEncoding];
+    NSData *replace_data = [replace dataUsingEncoding:NSASCIIStringEncoding];
+    
+    BOOL result = [self replaceData:search_data with:replace_data];
+    return result;
+}
+
+-(BOOL)wildcardReplaceData:(NSData *)search with:(NSData *)replace
+{
+    NSData *target_contents = [NSData dataWithContentsOfURL:[self target]];
+    NSArray *parts = [search componentsSeparatedByByte:0x2a]; // wildcard indicator is 0x2a (* ascii)
+    
+    int index = 0; // our position in the patch data
+    for (NSData *component in parts)
+    {
+        NSRange dataRange = [target_contents rangeOfData:component options:0
+                                                   range:NSMakeRange(0, [target_contents length])];
+        if (dataRange.location == NSNotFound)
+        {
+            return FALSE; // didn't find that part
+        }
+        else
+        {
+            // add 1 to length to cover the skipped wildcard
+            NSData *patchData = [replace subdataWithRange:NSMakeRange(index, (dataRange.length + 1))];
+            
+            NSError *error = NULL;
+            NSFileHandle *handle = [NSFileHandle fileHandleForWritingToURL:[self target] error:&error];
+            [handle seekToFileOffset:dataRange.location];
+            [handle writeData:patchData];
+            
+            [handle closeFile];
+            return TRUE;
+        }
+    }
 }
 
 @end
